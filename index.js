70083127d3370808402c6f05470da7b909b1099d
rknoche6 <rknoche@purdue.edu>
2021-12-08 23:47:42 +0100

    Set up the project and mongoose

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet")
+const morgan = require("morgan")
+
+dotenv.config();
+
+
+mongoose.connect('mongodb://localhost:27017/myapp');
+
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+
+app.listen(8800,()=>{
+    console.log("Backend server is running")
+})
